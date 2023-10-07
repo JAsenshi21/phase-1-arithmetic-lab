fb8a42eefb9bf52ab1118391470667d27fb78194
jeremy <jeremyaleman468@gmail.com>
2023-10-07 16:48:22 -0400

    complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num2 * num1;
+ 
+let random = Math.floor(Math.random()*10) + 1;
+
+let num3 = 16;
+let num4 = 4;
+
+let mod = num3 / num4;
+
+console.log(max);
+
+const set = [10, 5, 20 ,15];
+const max = Math.max(...set);
+
+
+
